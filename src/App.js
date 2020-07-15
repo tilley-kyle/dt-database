f683a3b45d28fcfe50aca10a7923574996262805
Kyle Tilley <katilley10@gmail.com>
2020-07-15 13:33:57 -0600

    added the spot for the body element changed the format of the overall page to a grid template

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import './App.css';
 import camproUSALogo from './images/Campro_USA_Blue_Logo.webp';
 import dummyData from './dummyData';
 
+import Results from './components/Results';
+
 class App extends React.Component {
   constructor(props) {
     super(props)
@@ -16,7 +18,7 @@ class App extends React.Component {
   }
 
   componentDidMount() {
-
+    this.setState({ currentResults: dummyData });
   }
 
   async onSearchInputClick(e) {
@@ -25,21 +27,26 @@ class App extends React.Component {
 
   render() {
     return (
-      <div className="banner-container">
-        <div className="contact-container">
-          <p className="call-today">Call Today: </p>
-          <p className="phone-number">(760) 706-7425</p>
-          {/* <a className="dealer" href="https://www.campro-usa.com/">Dealer Center</a> */}
-        </div>
-        <div className="banner">
-          <div className="search-input">
-            <div className="search item" id="search" onClick={(e) => this.onSearchInputClick(e)}>Search</div>
-            <div className="input item" id="input" onClick={(e) => this.onSearchInputClick(e)}>Input</div>
+      <div className="total-container">
+        <div className="banner-container">
+          <div className="contact-container">
+            <p className="call-today">Call Today: </p>
+            <p className="phone-number">(760) 706-7425</p>
+            {/* <a className="dealer" href="https://www.campro-usa.com/">Dealer Center</a> */}
           </div>
+          <div className="banner">
+            <div className="search-input">
+              <div className="search item" id="search" onClick={(e) => this.onSearchInputClick(e)}>Search</div>
+              <div className="input item" id="input" onClick={(e) => this.onSearchInputClick(e)}>Input</div>
+            </div>
+          </div>
+          <a href="https://www.campro-usa.com/">
+            <img className="banner-img" src={camproUSALogo} alt="campro usa logo" />
+          </a>
         </div>
-        <a href="https://www.campro-usa.com/">
-          <img className="banner-img" src={camproUSALogo} alt="campro usa logo" />
-        </a>
+        <div className="body=container">
+          <Results />
+        </div>
       </div>
     );
   }
