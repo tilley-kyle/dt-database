8a4e1f830ba1b15d6ffee6a88f1d689ef56d51e1
Kyle Tilley <katilley10@gmail.com>
2020-07-15 15:21:47 -0600

    have all of the data being put up onto the screen as it should

--- src/components/Machine.jsx
+++ src/components/Machine.jsx
@@ -9,76 +9,76 @@ const Machine = ({ machine })=> (
         <th className="value">Value</th>
       </tr>
       <tr>
-        <th className="title">Model</th>
-        <th>{machine}</th>
+        <td className="title">Model</td>
+        <td>{machine.Model}</td>
       </tr>
       <tr>
-        <th className="title">Serial Number</th>
-        <th>{}</th>
+        <td className="title">Serial Number</td>
+        <td>{machine['Serial-Number']}</td>
       </tr>
       <tr>
-        <th className="title">TW Invoice Number</th>
-        <th>{}</th>
+        <td className="title">TW Invoice Number</td>
+        <td>{machine['TW-Invoice-Number']}</td>
       </tr>
       <tr>
-        <th className="title">IW Invoice Date</th>
-        <th>{}</th>
+        <td className="title">TW Invoice Date</td>
+        <td>{machine['TW-Invoice-Date']}</td>
       </tr>
       <tr>
-        <th className="title">TW Invoice Total</th>
-        <th></th>
+        <td className="title">TW Invoice Total</td>
+        <td>{machine['TW-Invoice-Total']}</td>
       </tr>
       <tr>
-        <th className="title">Date Sold</th>
-        <th></th>
+        <td className="title">Date Sold</td>
+        <td>{machine['Date-Sold']}</td>
       </tr>
       <tr>
-        <th className="title">Dealer</th>
-        <th></th>
+        <td className="title">Dealer</td>
+        <td>{machine['Dealer']}</td>
       </tr>
       <tr>
-        <th className="title">End User</th>
-        <th></th>
+        <td className="title">End User</td>
+        <td>{machine['End-User']}</td>
       </tr>
       <tr>
-        <th className="title">CUSA Invoice Number</th>
-        <th></th>
+        <td className="title">CUSA Invoice Number</td>
+        <td>{machine['CUSA-Invoice-Number']}</td>
       </tr>
       <tr>
-        <th className="title">CUSA Invoice Date</th>
-        <th></th>
+        <td className="title">CUSA Invoice Date</td>
+        <td>{machine['CUSA-Invoice-Date']}</td>
       </tr>
       <tr>
-        <th className="title">CUSA Invoice Total</th>
-        <th></th>
+        <td className="title">CUSA Invoice Total</td>
+        <td>{machine['CUSA-Invoice-Total']}</td>
       </tr>
       <tr>
-        <th className="title">Date Paid</th>
-        <th></th>
+        <td className="title">Date Paid</td>
+        <td>{machine['Date-Paid']}</td>
       </tr>
       <tr>
-        <th className="title">Duty/Tarrif</th>
-        <th></th>
+        <td className="title">Duty/Tarrif</td>
+        <td>{machine['Duty/Tarrif']}</td>
       </tr>
       <tr>
-        <th className="title">Port Handling</th>
-        <th></th>
+        <td className="title">Port Handling</td>
+        <td>{machine['Port-Handling']}</td>
       </tr>
       <tr>
-        <th className="title">Devaning</th>
-        <th></th>
+        <td className="title">Devaning</td>
+        <td>{machine.Devaning}</td>
       </tr>
       <tr>
-        <th className="title">Other Handling</th>
-        <th></th>
+        <td className="title">Other Handling</td>
+        <td>{machine['Other-Handling']}</td>
       </tr>
       <tr>
-        <th className="title">Total Landed</th>
-        <th></th>
+        <td className="title">Total Landed</td>
+        <td>{machine['Total-Landed']}</td>
       </tr>
       <tr>
-        <th className="title">Gross Profit</th>
-        <th></th>
+        <td className="title">Gross Profit</td>
+        <td>{machine['Gross-Profit']}</td>
       </tr>
     </table>
   </div>
