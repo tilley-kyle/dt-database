94f30e8cc23181db2714d098f3f967826c5a9dd0
Kyle Tilley <katilley10@gmail.com>
2020-07-15 11:34:41 -0600

    added some dummy data to use during setup

--- src/App.js
+++ src/App.js
@@ -7,8 +7,14 @@ class App extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-
+      currentResults: {},
+      searchCriteria: 'total',
+      searchInput: '',
     }
+  }
+
+  componentDidMount() {
+
   }
 
   render() {
@@ -19,7 +25,12 @@ class App extends React.Component {
           <p className="phone-number">(760) 706-7425</p>
           {/* <a className="dealer" href="https://www.campro-usa.com/">Dealer Center</a> */}
         </div>
-        <div className="banner"></div>
+        <div className="banner">
+          <div className="search-input">
+            <div className="search-input-item">Search</div>
+            <div className="search-input-item">Input</div>
+          </div>
+        </div>
         <a href="https://www.campro-usa.com/">
           <img className="banner-img" src={camproUSALogo} alt="campro usa logo" />
         </a>
