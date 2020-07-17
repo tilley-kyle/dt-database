46fb3d88949305b0b17f414c06c743447e362f82
Kyle Tilley <katilley10@gmail.com>
2020-07-16 19:30:52 -0600

    input fields now show the whole word of the placeholders

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -3,24 +3,24 @@ import './components.css';
 
 const Input = ({ onNewMachineInput }) => (
   <div className="input-container">
-    <input className="input-field" type="input" placeholder="Enter Model" id="Model" onChange={(e) => onNewMachineInput(e)}/>
-    <input className="input-field" type="input" placeholder="Enter Serial Number" id="Serial-Number" onChange={(e) => onNewMachineInput(e)}/>
-    <input className="input-field" type="input" placeholder="Enter Taiwan Invoice Number" id="TW-Invoice-Number" onChange={(e) => onNewMachineInput(e)}/>
-    <input className="input-field" type="input" placeholder="Enter Taiwan Invoice Date" id="TW-Invoice-Date" onChange={(e) => onNewMachineInput(e)}/>
-    <input className="input-field" type="input" placeholder="Enter Taiwan Invoice Total" id="TW-Invoice-Total" onChange={(e) => onNewMachineInput(e)}/>
-    <input className="input-field" type="input" placeholder="Enter CamproUSA Invoice Number" id="CUSA-Invoice-Number" onChange={(e) => onNewMachineInput(e)}/>
-    <input className="input-field" type="input" placeholder="Enter CamproUSA Invoice Date" id="CUSA-Invoice-Date" onChange={(e) => onNewMachineInput(e)}/>
-    <input className="input-field" type="input" placeholder="Enter CamproUSA Invoice Total" id="CUSA-Invoice-Total" onChange={(e) => onNewMachineInput(e)}/>
-    <input className="input-field" type="input" placeholder="Enter Dealer" id="Dealer" onChange={(e) => onNewMachineInput(e)}/>
-    <input className="input-field" type="input" placeholder="Enter End User" id="End-User" onChange={(e) => onNewMachineInput(e)}/>
-    <input className="input-field" type="input" placeholder="Enter Date Sold" id="Date-Sold" onChange={(e) => onNewMachineInput(e)}/>
-    <input className="input-field" type="input" placeholder="Enter Date Paid" id="Date-Paid" onChange={(e) => onNewMachineInput(e)}/>
-    <input className="input-field" type="input" placeholder="Enter Duty/Tarrif" id="Duty/Tarrit" onChange={(e) => onNewMachineInput(e)}/>
-    <input className="input-field" type="input" placeholder="Enter Port Handling" id="Port-Handling" onChange={(e) => onNewMachineInput(e)}/>
-    <input className="input-field" type="input" placeholder="Enter Devaning" id="Devaning" onChange={(e) => onNewMachineInput(e)}/>
-    <input className="input-field" type="input" placeholder="Enter Other Handling" id="Other-Handling" onChange={(e) => onNewMachineInput(e)}/>
-    <input className="input-field" type="input" placeholder="" id="" onChange={(e) => onNewMachineInput(e)}/>
-    <input className="input-field" type="input" placeholder="" id="" onChange={(e) => onNewMachineInput(e)}/>
+    <div className="field-container">
+      <input className="input-field" type="input" placeholder="Model" id="Model" onChange={(e) => onNewMachineInput(e)} />
+      <input className="input-field" type="input" placeholder="Serial Number" id="Serial-Number" onChange={(e) => onNewMachineInput(e)} />
+      <input className="input-field" type="input" placeholder="Taiwan Invoice Number" id="TW-Invoice-Number" onChange={(e) => onNewMachineInput(e)} />
+      <input className="input-field" type="input" placeholder="Taiwan Invoice Date" id="TW-Invoice-Date" onChange={(e) => onNewMachineInput(e)} />
+      <input className="input-field" type="input" placeholder="Taiwan Invoice Total" id="TW-Invoice-Total" onChange={(e) => onNewMachineInput(e)} />
+      <input className="input-field" type="input" placeholder="CamproUSA Invoice Number" id="CUSA-Invoice-Number" onChange={(e) => onNewMachineInput(e)} />
+      <input className="input-field" type="input" placeholder="CamproUSA Invoice Date" id="CUSA-Invoice-Date" onChange={(e) => onNewMachineInput(e)} />
+      <input className="input-field" type="input" placeholder="CamproUSA Invoice Total" id="CUSA-Invoice-Total" onChange={(e) => onNewMachineInput(e)} />
+      <input className="input-field" type="input" placeholder="Dealer" id="Dealer" onChange={(e) => onNewMachineInput(e)} />
+      <input className="input-field" type="input" placeholder="End User" id="End-User" onChange={(e) => onNewMachineInput(e)} />
+      <input className="input-field" type="input" placeholder="Date Sold" id="Date-Sold" onChange={(e) => onNewMachineInput(e)} />
+      <input className="input-field" type="input" placeholder="Date Paid" id="Date-Paid" onChange={(e) => onNewMachineInput(e)} />
+      <input className="input-field" type="input" placeholder="Duty/Tarrif" id="Duty/Tarrit" onChange={(e) => onNewMachineInput(e)} />
+      <input className="input-field" type="input" placeholder="Port Handling" id="Port-Handling" onChange={(e) => onNewMachineInput(e)} />
+      <input className="input-field" type="input" placeholder="Devaning" id="Devaning" onChange={(e) => onNewMachineInput(e)} />
+      <input className="input-field" type="input" placeholder="Other Handling" id="Other-Handling" onChange={(e) => onNewMachineInput(e)} />
+    </div>
   </div>
 );
 
