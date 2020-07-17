c61a2d0f7f49ac1f720209c99395cdb6033604c5
Kyle Tilley <katilley10@gmail.com>
2020-07-17 14:00:18 -0600

    all info is being found properly, now I need to reformat the whole thing

--- src/components/Machine.jsx
+++ src/components/Machine.jsx
@@ -3,7 +3,29 @@ import './components.css';
 
 const Machine = ({ machine }) => (
   <div className='machine-container'>
-    <table>
+    <tbody>
+      <tr>
+        <td>{machine['Model']}</td>
+        <td>{machine['Serial-Number']}</td>
+        <td>{machine['TW-Invoice-Number']}</td>
+        <td>{machine['TW-Invoice-Date']}</td>
+        <td>{machine['TW-Invoice-Total']}</td>
+        <td>{machine['CUSA-Invoice-Number']}</td>
+        <td>{machine['CUSA-Invoice-Date']}</td>
+        <td>{machine['CUSA-Invoice-Total']}</td>
+        <td>{machine['Dealer']}</td>
+        <td>{machine['End-User']}</td>
+        <td>{machine['Date-Sold']}</td>
+        <td>{machine['Date-Paid']}</td>
+        <td>{machine['Duty/Tarrif']}</td>
+        <td>{machine['Port-Handling']}</td>
+        <td>{machine['Devaning']}</td>
+        <td>{machine['Other-Handling']}</td>
+        <td>{machine['Total-Landed']}</td>
+        <td>{machine['Gross-Profit']}</td>
+      </tr>
+    </tbody>
+    {/* <table>
       <thead>
         <tr>
           <th className="field">Field</th>
@@ -84,7 +106,7 @@ const Machine = ({ machine }) => (
         <td>${machine['Gross-Profit']}</td>
       </tr>
       </tbody>
-    </table>
+    </table> */}
   </div>
 );
 
--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -8,10 +8,32 @@ const Results = ({ currentResults, onSearchClick, onTextInput }) => {
   return (
     <div className="results-container">
       <SearchBar onSearchClick={onSearchClick} onTextInput={onTextInput} />
-      <div className="table-container">
+      <table className="table-container">
+      <thead>
+        <tr>
+          <th>Model</th>
+          <th>Serial Number</th>
+          <th>TW Invoice Number</th>
+          <th>TW Invoice Date</th>
+          <th>TW Invoice Total</th>
+          <th>CUSA Invoice Number</th>
+          <th>CUSA Invoice Date</th>
+          <th>CUSA Invoice Total</th>
+          <th>Dealer</th>
+          <th>End User</th>
+          <th>Date Sold</th>
+          <th>Date Paid</th>
+          <th>Duty/Tarrif</th>
+          <th>Port Handling</th>
+          <th>Devaning</th>
+          <th>Other Handling</th>
+          <th>Total Landed</th>
+          <th>Gross Profit</th>
+        </tr>
+      </thead>
         {currentResults.map((machine) => <Machine key={machine.Model} machine={machine} />
         )}
-      </div>
+      </table>
     </div>
   )
 };
