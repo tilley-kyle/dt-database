8046a18c7a932862d2be09c89b90c29a6b4a612a
Kyle Tilley <katilley10@gmail.com>
2020-07-16 18:23:38 -0600

    now searchng by comapny works as well

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -7,9 +7,9 @@ const SearchBar = ({ onSearchClick, onTextInput }) => (
     <button type="submit" value="Model" onClick={(e) => {onSearchClick(e)}}>Search Model</button>
     <input type="input" placeholder="Input Serial ID" onChange={(e) => {onTextInput(e)}}></input>
     <button type="submit" value="Serial-Number" onClick={(e) => {onSearchClick(e)}}>Search Serial</button>
-    <input type="input" placeholder="Input Dealer"></input>
+    <input type="input" placeholder="Input Dealer" onChange={(e) => {onTextInput(e)}}></input>
     <button type="submit" value="Dealer" onClick={(e) => {onSearchClick(e)}}>Search Dealer</button>
-    <input type="input" placeholder="Input End User"></input>
+    <input type="input" placeholder="Input End User" onChange={(e) => {onTextInput(e)}}></input>
     <button type="submit" value="End-User" onClick={(e) => {onSearchClick(e)}}>Search End User</button>
     <button type="submit" value="all" onClick={(e) => {onSearchClick(e)}}>All Sales</button>
   </div>
