f509337f152794fd74304185304cd9beb65a98cd
Kyle Tilley <katilley10@gmail.com>
2020-07-17 19:33:59 -0600

    fixed the bug messing up formatting

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ class App extends React.Component {
   }
 
   componentDidMount() {
-    this.setState({ totalResults: dummyData, currentResults: dummyData });
+    this.setState({ totalResults: dummyData });
   }
 
   async onSearchInputClick(e) {
