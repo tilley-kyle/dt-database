19d2ed9a3d831a0017fd9757d2eeec76e7b25b9f
Kyle Tilley <katilley10@gmail.com>
2020-07-20 12:02:27 -0600

    have plan for function to work

--- src/helperFunctions/inputFormattingController.js
+++ src/helperFunctions/inputFormattingController.js
@@ -1,5 +1,14 @@
-const inputFormattingController = (inputObj) => {
 
+
+const inputFormattingController = (inputObj) => {
+  //obj that has each field
+  //obj that keeps track of each field that is wrong
+  //iterate throught object
+    //check that items formatting
+    //if supposed to be a dollar amount, run it through number converter
+    //if supposed to be date, foromat it to mm/dd/yyyy
+  //if obj that keeps track of wrong fields is empty, return fomratted inputObj
+  //else, return field names that are wrong
 };
 
 export default inputFormattingController
