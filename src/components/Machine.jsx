35a66baf31001920d54b2a97db4c9a154e49f5bd
Kyle Tilley <katilley10@gmail.com>
2020-07-20 09:57:59 -0600

    changed order of fields for the client

--- src/components/Machine.jsx
+++ src/components/Machine.jsx
@@ -11,18 +11,18 @@ const Machine = ({ machine, inc }) => {
         <td className="data-entry">{machine['TW-Invoice-Number']}</td>
         <td className="data-entry">{machine['TW-Invoice-Date']}</td>
         <td className="data-entry">${machine['TW-Invoice-Total']}</td>
-        <td className="data-entry">{machine['CUSA-Invoice-Number']}</td>
-        <td className="data-entry">{machine['CUSA-Invoice-Date']}</td>
-        <td className="data-entry">${machine['CUSA-Invoice-Total']}</td>
-        <td className="data-entry">{machine['Dealer']}</td>
-        <td className="data-entry">{machine['End-User']}</td>
-        <td className="data-entry">{machine['Date-Sold']}</td>
-        <td className="data-entry">{machine['Date-Paid']}</td>
         <td className="data-entry">${machine['Duty/Tarrif']}</td>
         <td className="data-entry">${machine['Port-Handling']}</td>
         <td className="data-entry">${machine['Devaning']}</td>
         <td className="data-entry">${machine['Other-Handling']}</td>
         <td className="data-entry">${machine['Total-Landed']}</td>
+        <td className="data-entry">{machine['Dealer']}</td>
+        <td className="data-entry">{machine['End-User']}</td>
+        <td className="data-entry">{machine['CUSA-Invoice-Number']}</td>
+        <td className="data-entry">{machine['CUSA-Invoice-Date']}</td>
+        <td className="data-entry">${machine['CUSA-Invoice-Total']}</td>
+        <td className="data-entry">{machine['Date-Sold']}</td>
+        <td className="data-entry">{machine['Date-Paid']}</td>
         <td className="data-entry">${machine['Gross-Profit']}</td>
       </tr>
     </tbody>
--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -17,18 +17,18 @@ const Results = ({ currentResults, onSearchClick, onTextInput }) => {
             <th className="title">TW Invoice Number</th>
             <th className="title">TW Invoice Date</th>
             <th className="title">TW Invoice Total</th>
-            <th className="title">CUSA Invoice Number</th>
-            <th className="title">CUSA Invoice Date</th>
-            <th className="title">CUSA Invoice Total</th>
-            <th className="title">Dealer</th>
-            <th className="title">End User</th>
-            <th className="title">Date Sold</th>
-            <th className="title">Date Paid</th>
             <th className="title">Duty/ Tarrif</th>
             <th className="title">Port Handling</th>
             <th className="title">Devaning</th>
             <th className="title">Other Handling</th>
             <th className="title">Total Landed</th>
+            <th className="title">Dealer</th>
+            <th className="title">End User</th>
+            <th className="title">CUSA Invoice Number</th>
+            <th className="title">CUSA Invoice Date</th>
+            <th className="title">CUSA Invoice Total</th>
+            <th className="title">Date Sold</th>
+            <th className="title">Date Paid</th>
             <th className="title">Gross Profit</th>
           </tr>
         </thead>
