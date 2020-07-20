4969189f1611eaf93a21c2be1f375e72e0b321da
Kyle Tilley <katilley10@gmail.com>
2020-07-20 14:52:08 -0600

    added feature to delete the input field when onClick for a search

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -3,14 +3,14 @@ import './components.css';
 
 const SearchBar = ({ onSearchClick, onTextInput }) => (
   <div className="search-bar">
-    <input type="input" placeholder="Input Model ID" onChange={(e) => {onTextInput(e)}}></input>
-    <button type="submit" value="Model" onClick={(e) => {onSearchClick(e)}}>Search Model</button>
-    <input type="input" placeholder="Input Serial ID" onChange={(e) => {onTextInput(e)}}></input>
-    <button type="submit" value="Serial-Number" onClick={(e) => {onSearchClick(e)}}>Search Serial</button>
-    <input type="input" placeholder="Input Dealer" onChange={(e) => {onTextInput(e)}}></input>
-    <button type="submit" value="Dealer" onClick={(e) => {onSearchClick(e)}}>Search Dealer</button>
-    <input type="input" placeholder="Input End User" onChange={(e) => {onTextInput(e)}}></input>
-    <button type="submit" value="End-User" onClick={(e) => {onSearchClick(e)}}>Search End User</button>
+    <input type="input" id="searchInputModel" placeholder="Input Model ID" onChange={(e) => {onTextInput(e)}}></input>
+    <button type="submit" value="Model" onClick={(e) => {onSearchClick(e); document.getElementById('searchInputModel').value=''}}>Search Model</button>
+    <input type="input" id="searchInputSerial" placeholder="Input Serial ID" onChange={(e) => {onTextInput(e)}}></input>
+    <button type="submit" value="Serial-Number" onClick={(e) => {onSearchClick(e); document.getElementById('searchInputSerial').value=''}}>Search Serial</button>
+    <input type="input" id="searchInputDealer" placeholder="Input Dealer" onChange={(e) => {onTextInput(e)}}></input>
+    <button type="submit" value="Dealer" onClick={(e) => {onSearchClick(e); document.getElementById('searchInputDealer').value=''}}>Search Dealer</button>
+    <input type="input" id="searchInputUser" placeholder="Input End User" onChange={(e) => {onTextInput(e)}}></input>
+    <button type="submit" value="End-User" onClick={(e) => {onSearchClick(e); document.getElementById('searchInputUser').value=''}}>Search End User</button>
     <button type="submit" value="all" onClick={(e) => {onSearchClick(e)}}>All Sales</button>
   </div>
 );
