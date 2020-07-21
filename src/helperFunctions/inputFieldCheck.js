afce3ccc8cd348b1a1da67cc59935942fa409dd8
Kyle Tilley <katilley10@gmail.com>
2020-07-21 15:12:39 -0600

    created an input checker

---
+++ src/helperFunctions/inputFieldCheck.js
@@ -0,0 +1,33 @@
+const inputFieldCheck = (inputObject) => {
+  const missingFields = [];
+  const fields = [
+    'TW_Invoice_Total',
+    'Duty_Tarrif',
+    'Port_Handling',
+    'Devaning',
+    'Other_Handling',
+    'Total_Landed',
+    'CUSA_Invoice_Total',
+    'Gross_Profit',
+    'Model',
+    'Serial_Number',
+    'Dealer',
+    'End_User',
+    'CUSA_Invoice_Date',
+    'CUSA_Invoice_Number',
+    'Date_Paid',
+    'TW_Invoice_Number',
+    'TW_Invoice_Date'
+    ];
+  fields.map((field) => {
+    if (!inputObject[field]) {
+      missingFields.push(field);
+    }
+  });
+  if (missingFields.length === 0) {
+    return missingFields;
+  }
+  return false;
+};
+
+export default inputFieldCheck;
