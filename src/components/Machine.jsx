d50eb0543d412344fd183225708f4e18c1b6b2e8
Kyle Tilley <katilley10@gmail.com>
2020-07-21 14:40:17 -0600

    fixed dashes vs underscores in machine.jsx

--- src/components/Machine.jsx
+++ src/components/Machine.jsx
@@ -7,22 +7,22 @@ const Machine = ({ machine, inc }) => {
     <tbody>
       <tr className={tdClassName}>
         <td className="data-entry">{machine['Model']}</td>
-        <td className="data-entry">{machine['Serial-Number']}</td>
-        <td className="data-entry">{machine['TW-Invoice-Number']}</td>
-        <td className="data-entry">{machine['TW-Invoice-Date']}</td>
-        <td className="data-entry">${machine['TW-Invoice-Total']}</td>
-        <td className="data-entry">${machine['Duty/Tarrif']}</td>
-        <td className="data-entry">${machine['Port-Handling']}</td>
+        <td className="data-entry">{machine['Serial_Number']}</td>
+        <td className="data-entry">{machine['TW_Invoice_Number']}</td>
+        <td className="data-entry">{machine['TW_Invoice_Date']}</td>
+        <td className="data-entry">${machine['TW_Invoice_Total']}</td>
+        <td className="data-entry">${machine['Duty_Tarrif']}</td>
+        <td className="data-entry">${machine['Port_Handling']}</td>
         <td className="data-entry">${machine['Devaning']}</td>
-        <td className="data-entry">${machine['Other-Handling']}</td>
-        <td className="data-entry">${machine['Total-Landed']}</td>
+        <td className="data-entry">${machine['Other_Handling']}</td>
+        <td className="data-entry">${machine['Total_Landed']}</td>
         <td className="data-entry">{machine['Dealer']}</td>
-        <td className="data-entry">{machine['End-User']}</td>
-        <td className="data-entry">{machine['CUSA-Invoice-Number']}</td>
-        <td className="data-entry">{machine['CUSA-Invoice-Date']}</td>
-        <td className="data-entry">${machine['CUSA-Invoice-Total']}</td>
-        <td className="data-entry">{machine['Date-Paid']}</td>
-        <td className="data-entry">${machine['Gross-Profit']}</td>
+        <td className="data-entry">{machine['End_User']}</td>
+        <td className="data-entry">{machine['CUSA_Invoice_Number']}</td>
+        <td className="data-entry">{machine['CUSA_Invoice_Date']}</td>
+        <td className="data-entry">${machine['CUSA_Invoice_Total']}</td>
+        <td className="data-entry">{machine['Date_Paid']}</td>
+        <td className="data-entry">${machine['Gross_Profit']}</td>
       </tr>
     </tbody>
 
