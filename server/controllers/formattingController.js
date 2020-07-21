7045d54180ddab33c362bfb9449b99601d25164c
Kyle Tilley <katilley10@gmail.com>
2020-07-21 16:47:49 -0600

    added the info into the database from the formattingController

--- server/controllers/formattingController.js
+++ server/controllers/formattingController.js
@@ -12,7 +12,26 @@ const formattingController = async (req, res) => {
   dateFields.forEach((field) => {
     req.body[field] = dateFormatter(req.body[field]);
   });
-  console.log(req.body)
+  const machine = new Machine({
+    Model: req.body['Model'],
+    Serial_Number: req.body['Serial_Number'],
+    TW_Invoice_Number: req.body['TW_Invoice_Number'],
+    TW_Invoice_Date: req.body['TW_Invoice_Date'],
+    TW_Invoice_Total: req.body['TW_Invoice_Total'],
+    CUSA_Invoice_Number: req.body['CUSA_Invoice_Number'],
+    CUSA_Invoice_Date: req.body['CUSA_Invoice_Date'],
+    CUSA_Invoice_Total: req.body['CUSA_Invoice_Total'],
+    Dealer: req.body['Dealer'],
+    End_User: req.body['End_User'],
+    Date_Paid: req.body['Date_Paid'],
+    Duty_Tarrif: req.body['Duty_Tarrif'],
+    Port_Handling: req.body['Port_Handling'],
+    Devaning: req.body['Devaning'],
+    Other_Handling: req.body['Other_Handling'],
+    Total_Landed: req.body['Total_Landed'],
+    Gross_Profit: req.body['Gross_Profit'],
+  });
+  machine.save();
 };
 
 module.exports = formattingController;
