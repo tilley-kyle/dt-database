46283948f2ffafce548d654b42518fc004239036
Kyle Tilley <katilley10@gmail.com>
2020-07-21 14:41:06 -0600

    fixed searchbar.jsw to underscores

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -6,11 +6,11 @@ const SearchBar = ({ onSearchClick, onTextInput }) => (
     <input type="input" id="searchInputModel" placeholder="Input Model ID" onChange={(e) => {onTextInput(e)}}></input>
     <button type="submit" value="Model" onClick={(e) => {onSearchClick(e); document.getElementById('searchInputModel').value=''}}>Search Model</button>
     <input type="input" id="searchInputSerial" placeholder="Input Serial ID" onChange={(e) => {onTextInput(e)}}></input>
-    <button type="submit" value="Serial-Number" onClick={(e) => {onSearchClick(e); document.getElementById('searchInputSerial').value=''}}>Search Serial</button>
+    <button type="submit" value="Serial_Number" onClick={(e) => {onSearchClick(e); document.getElementById('searchInputSerial').value=''}}>Search Serial</button>
     <input type="input" id="searchInputDealer" placeholder="Input Dealer" onChange={(e) => {onTextInput(e)}}></input>
     <button type="submit" value="Dealer" onClick={(e) => {onSearchClick(e); document.getElementById('searchInputDealer').value=''}}>Search Dealer</button>
     <input type="input" id="searchInputUser" placeholder="Input End User" onChange={(e) => {onTextInput(e)}}></input>
-    <button type="submit" value="End-User" onClick={(e) => {onSearchClick(e); document.getElementById('searchInputUser').value=''}}>Search End User</button>
+    <button type="submit" value="End_User" onClick={(e) => {onSearchClick(e); document.getElementById('searchInputUser').value=''}}>Search End User</button>
     <button type="submit" value="all" onClick={(e) => {onSearchClick(e)}}>All Sales</button>
   </div>
 );
