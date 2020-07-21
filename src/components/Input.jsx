28a85dddd900be00218d0ec1e62adbafc0eb1627
Kyle Tilley <katilley10@gmail.com>
2020-07-21 14:39:05 -0600

    switched out all ids to use underscores instead of dashes to meet what the schema is going to be looking for

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -11,20 +11,20 @@ const Input = ({ onNewMachineInput, newInput, onSubmitData }) => {
     <div className="input-container">
       <div className="field-container">
         <input className="input-field" type="input" placeholder="Model" id="Model" onChange={(e) => onNewMachineInput(e)} />
-        <input className="input-field" type="input" placeholder="Serial Number" id="Serial-Number" onChange={(e) => onNewMachineInput(e)} />
-        <input className="input-field" type="input" placeholder="Taiwan Invoice Number" id="TW-Invoice-Number" onChange={(e) => onNewMachineInput(e)} />
-        <input className="input-field" type="date" id="TW-Invoice-Date" onChange={(e) => onNewMachineInput(e)} />
-        <input className="input-field" type="input" placeholder="Taiwan Invoice Total" id="TW-Invoice-Total" onChange={(e) => onNewMachineInput(e)} />
-        <input className="input-field" type="input" placeholder="CUSA Invoice Number" id="CUSA-Invoice-Number" onChange={(e) => onNewMachineInput(e)} />
-        <input className="input-field" type="date" id="CUSA-Invoice-Date" onChange={(e) => onNewMachineInput(e)} />
-        <input className="input-field" type="input" placeholder="CUSA Invoice Total" id="CUSA-Invoice-Total" onChange={(e) => onNewMachineInput(e)} />
+        <input className="input-field" type="input" placeholder="Serial Number" id="Serial_Number" onChange={(e) => onNewMachineInput(e)} />
+        <input className="input-field" type="input" placeholder="Taiwan Invoice Number" id="TW_Invoice_Number" onChange={(e) => onNewMachineInput(e)} />
+        <input className="input-field" type="date" id="TW_Invoice_Date" onChange={(e) => onNewMachineInput(e)} />
+        <input className="input-field" type="input" placeholder="Taiwan Invoice Total" id="TW_Invoice_Total" onChange={(e) => onNewMachineInput(e)} />
+        <input className="input-field" type="input" placeholder="CUSA Invoice Number" id="CUSA_Invoice_Number" onChange={(e) => onNewMachineInput(e)} />
+        <input className="input-field" type="date" id="CUSA_Invoice_Date" onChange={(e) => onNewMachineInput(e)} />
+        <input className="input-field" type="input" placeholder="CUSA Invoice Total" id="CUSA_Invoice_Total" onChange={(e) => onNewMachineInput(e)} />
         <input className="input-field" type="input" placeholder="Dealer" id="Dealer" onChange={(e) => onNewMachineInput(e)} />
-        <input className="input-field" type="input" placeholder="End User" id="End-User" onChange={(e) => onNewMachineInput(e)} />
-        <input className="input-field" type="date" id="Date-Paid" onChange={(e) => onNewMachineInput(e)} />
-        <input className="input-field" type="input" placeholder="Duty/Tarrif" id="Duty/Tarrif" onChange={(e) => onNewMachineInput(e)} />
-        <input className="input-field" type="input" placeholder="Port Handling" id="Port-Handling" onChange={(e) => onNewMachineInput(e)} />
+        <input className="input-field" type="input" placeholder="End User" id="End_User" onChange={(e) => onNewMachineInput(e)} />
+        <input className="input-field" type="date" id="Date_Paid" onChange={(e) => onNewMachineInput(e)} />
+        <input className="input-field" type="input" placeholder="Duty/Tarrif" id="Duty_Tarrif" onChange={(e) => onNewMachineInput(e)} />
+        <input className="input-field" type="input" placeholder="Port Handling" id="Port_Handling" onChange={(e) => onNewMachineInput(e)} />
         <input className="input-field" type="input" placeholder="Devaning" id="Devaning" onChange={(e) => onNewMachineInput(e)} />
-        <input className="input-field" type="input" placeholder="Other Handling" id="Other-Handling" onChange={(e) => onNewMachineInput(e)} />
+        <input className="input-field" type="input" placeholder="Other Handling" id="Other_Handling" onChange={(e) => onNewMachineInput(e)} />
       </div>
       <div className="total-submit">
         <p className="totals">Total Landed: ${parseInt(totalLanded).toFixed(2)}</p>
