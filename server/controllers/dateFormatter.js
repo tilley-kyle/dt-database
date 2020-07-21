636a195f4ae5c46dba6892195ab0bb31f6388030
Kyle Tilley <katilley10@gmail.com>
2020-07-21 16:34:38 -0600

    added in the machine schema to the formatting controller

--- server/controllers/dateFormatter.js
+++ server/controllers/dateFormatter.js
@@ -1,7 +1,9 @@
 const dateFormatter = (inputDate) => {
   const date = new Date();
   const year = date.getFullYear();
-  if (parseInt(inputDate.split('-')[0]) < 2000 || parseInt(inputDate.split('-')[0]) > year + 1) return false
+  if (parseInt(inputDate.split('-')[0]) < 2000 || parseInt(inputDate.split('-')[0]) > year + 1) {
+    return false
+  }
   inputDate = inputDate.split('-');
   return `${inputDate[1]}/${inputDate[2]}/${inputDate[0]}`;
 };
--- server/controllers/formattingController.js
+++ server/controllers/formattingController.js
@@ -1,7 +1,9 @@
 const currencyFormatter = require('./currencyFormatter');
 const dateFormatter = require('./dateFormatter');
 
-const formattingController = (req, res) => {
+const Machine = require('../../db/machineSchema');
+
+const formattingController = async (req, res) => {
   const currencyFields = ['TW_Invoice_Total', 'Duty_Tarrif', 'Port_Handling', 'Devaning', 'Other_Handling', 'Total_Landed', 'CUSA_Invoice_Total', 'Gross_Profit'];
   const dateFields = ['TW_Invoice_Date', 'CUSA_Invoice_Date', 'Date_Paid'];
   currencyFields.forEach((field) => {
