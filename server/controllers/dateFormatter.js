fc3201cbf6b831e42c054c9469696fb45b7fbb91
Kyle Tilley <katilley10@gmail.com>
2020-07-21 16:30:14 -0600

    fixed date formatting function

--- server/controllers/dateFormatter.js
+++ server/controllers/dateFormatter.js
@@ -2,7 +2,8 @@ const dateFormatter = (inputDate) => {
   const date = new Date();
   const year = date.getFullYear();
   if (parseInt(inputDate.split('-')[0]) < 2000 || parseInt(inputDate.split('-')[0]) > year + 1) return false
-  return inputDate;
+  inputDate = inputDate.split('-');
+  return `${inputDate[1]}/${inputDate[2]}/${inputDate[0]}`;
 };
 
 module.exports =  dateFormatter;
