6d47f70184bd99ccd966d5333173e88f20424cd9
Kyle Tilley <katilley10@gmail.com>
2020-07-22 12:19:07 -0600

    refactored dateFormatter file to pass all tests

--- server/controllers/dateFormatter.js
+++ server/controllers/dateFormatter.js
@@ -1,10 +1,16 @@
 const dateFormatter = (inputDate) => {
   const date = new Date();
   const year = date.getFullYear();
-  if (parseInt(inputDate.split('-')[0]) < 2000 || parseInt(inputDate.split('-')[0]) > year + 1) {
-    return false
-  }
   inputDate = inputDate.split('-');
+  if (!inputDate[0]) {
+    return false;
+  } else if (parseInt(inputDate[0]) < 2000 || parseInt(inputDate[0]) > year + 1) {
+    return false;
+  } else if(parseInt(inputDate[1]) < 1 || parseInt(inputDate[1]) > 12) {
+    return false;
+  } else if(parseInt(inputDate[2]) < 1 || parseInt(inputDate[2]) > 31) {
+    return false;
+  }
   return `${inputDate[1]}/${inputDate[2]}/${inputDate[0]}`;
 };
 
