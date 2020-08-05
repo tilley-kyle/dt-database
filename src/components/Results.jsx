70a0bd2561ae179bc9b3fb894f118a0fbb86a324
Kyle Tilley <katilley10@gmail.com>
2020-08-05 13:08:32 -0600

    have my average and total function working in the new table above the results box

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -11,40 +11,63 @@ const Results = ({ currentResults, onSearchClick, onTextInput, onEditClick }) =>
         <SearchBar onSearchClick={onSearchClick} onTextInput={onTextInput} />
       </div>
     )
+  } else {
+    let totalProfit = 0;
+    currentResults.forEach((machine) => {
+      totalProfit += parseInt(machine['Gross_Profit']);
+    });
+    return (
+      <div className="results-container">
+        <SearchBar onSearchClick={onSearchClick} onTextInput={onTextInput} />
+        <div className="machine-container">
+          <table className="table-container">
+            <thead>
+              <tr className="title-box">
+                <th className="title">Total Sold</th>
+                <th className="title">Total Profit</th>
+                <th className="title">Average Profit</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr className="even">
+                <td className="data-entry">{currentResults.length}</td>
+                <td className="data-entry">{totalProfit.toLocaleString("en-US", {style:"currency", currency:"USD"})}</td>
+                <td className="data-entry">
+                  {(totalProfit / currentResults.length).toLocaleString("en-US", {style:"currency", currency:"USD"})}
+                </td>
+              </tr>
+            </tbody>
+          </table>
+          <table className="table-container">
+            <thead>
+              <tr className="title-box">
+                <th className=""></th>
+                <th className="title">Model</th>
+                <th className="title">Serial Number</th>
+                <th className="title">TW Invoice Number</th>
+                <th className="title">TW Invoice Date</th>
+                <th className="title">TW Invoice Total</th>
+                <th className="title">Duty/ Tarrif</th>
+                <th className="title">Port Handling</th>
+                <th className="title">Devaning</th>
+                <th className="title">Other Handling</th>
+                <th className="title">Total Landed</th>
+                <th className="title">Dealer</th>
+                <th className="title">End User</th>
+                <th className="title">CUSA Invoice Number</th>
+                <th className="title">CUSA Invoice Date</th>
+                <th className="title">CUSA Invoice Total</th>
+                <th className="title">Date Paid</th>
+                <th className="title">Gross Profit</th>
+              </tr>
+            </thead>
+            {currentResults.map((machine, i) => <Machine key={i} inc={i} machine={machine} onEditClick={onEditClick} />
+            )}
+          </table>
+        </div>
+      </div>
+    )
   }
-  return (
-    <div className="results-container">
-      <SearchBar onSearchClick={onSearchClick} onTextInput={onTextInput} />
-      <div className="machine-container">
-      <table className="table-container">
-        <thead>
-          <tr className="title-box">
-            <th className=""></th>
-            <th className="title">Model</th>
-            <th className="title">Serial Number</th>
-            <th className="title">TW Invoice Number</th>
-            <th className="title">TW Invoice Date</th>
-            <th className="title">TW Invoice Total</th>
-            <th className="title">Duty/ Tarrif</th>
-            <th className="title">Port Handling</th>
-            <th className="title">Devaning</th>
-            <th className="title">Other Handling</th>
-            <th className="title">Total Landed</th>
-            <th className="title">Dealer</th>
-            <th className="title">End User</th>
-            <th className="title">CUSA Invoice Number</th>
-            <th className="title">CUSA Invoice Date</th>
-            <th className="title">CUSA Invoice Total</th>
-            <th className="title">Date Paid</th>
-            <th className="title">Gross Profit</th>
-          </tr>
-        </thead>
-        {currentResults.map((machine, i) => <Machine key={i} inc={i} machine={machine} onEditClick={onEditClick} />
-        )}
-      </table>
-    </div>
-    </div>
-  )
 };
 
 export default Results;
